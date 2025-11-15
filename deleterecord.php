9a4a8ef0849c1b83c7129ee086dbfff7b3447214
diya p <diyaapatel@algomau.ca>
2025-11-15 01:01:06 -0500

    lab done

---
+++ deleterecord.php
@@ -0,0 +1,62 @@
+<?php 
+require_once './includes/header.php';
+require_once './db/conn.php';
+
+// Handle deletion if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['client_id'])) {
+$client_id = mysqli_real_escape_string($conn, $_POST['client_id']);
+
+$delete_sql = "DELETE FROM client_info WHERE client_id = '$client_id'";
+
+if (mysqli_query($conn, $delete_sql)) {
+    if (mysqli_affected_rows($conn) > 0) {
+        echo "Record deleted! <br>";
+    } else {
+        echo "Client ID not found.<br>";
+    }
+} else {
+    echo "Error deleting record: " . mysqli_error($conn) . "<br>";
+}
+}
+
+// Fetch all records to display
+$sql = "SELECT * FROM client_info WHERE 1";
+$result = mysqli_query($conn, $sql);
+?>
+
+<h2>Delete Records</h2>
+<a href="index.php">Back to Form</a>
+
+<h3>Enter ID to delete it:</h3>
+<form method="post" action="deleterecord.php">
+    <label>Client ID: </label>
+    <input type="number" name="client_id" required>
+    <button type="submit">Delete</button>
+</form>
+
+<hr>
+
+<h3>Current Records:</h3>
+
+<?php
+if($result) {
+    if(mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            echo $row["client_id"] . " ";
+            echo $row["email"] . " ";
+            echo $row["address"] . " ";
+            echo $row["city"] . " ";
+            echo $row["province"] . " ";
+            echo $row["postal_code"] . "<br>";
+        }
+    } else {
+        echo "No records found";
+    }
+} else {
+    echo "Error executing query: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+
+<?php require_once './includes/footer.php'; ?>
